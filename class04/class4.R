9cf2bac4c8367a0bce28880f492edb9bd42ad3b3
thrishapraveen <156376838+thrishapraveen@users.noreply.github.com>
2025-03-04 10:55:39 -0800

    Labs 4,5,6,6 HW,7 added

---
+++ class04/class4.R
@@ -0,0 +1,13 @@
+#' ---
+#' title: "Lab 4 PDF Report - R Script"
+#' output: "pdf_document"
+#' author: "Thrisha Praveen"
+#' date: "January 16th, 2025"
+#' ---
+
+x <- 1:50
+plot(x, sin(x))
+
+#Make this nicer
+plot(x, sin(x), col="blue", type="l", lwd=3)
+
